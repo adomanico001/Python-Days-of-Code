4a24599f38ad70c611849d46f103f73eb7027524
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-01-11 12:58:08 -0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+# @author - Addie Domanico
+# @version - 01/10/2024
+
+# Create a program that swaps the values of two variables
+
+# Receive user input for two variables
+value1 = input("Enter the first value (number or letter): ")
+value2 = input("Enter the second value (number or letter): ")
+
+print("The inital values include: " + value1 + " and " + value2)
+
+# New variable
+new = value1
+value1 = value2
+value2 = new
+
+print("The values swapped are: " + value1 + " and " + value2)
+
