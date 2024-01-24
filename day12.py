9e24d7abc094736a533b90637d36979b767dab8b
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-01-24 14:43:17 -0500

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,10 @@
+# Write a program to reverse a given string
+
+# @author - Addie Domanico
+# @version - 01/21/2024
+
+user_input = input("Enter a short sentence (and then have it reversed!): ")
+
+reversed_string = user_input[::-1]
+
+print(f"Here is your sentence reversed: {reversed_string}")
